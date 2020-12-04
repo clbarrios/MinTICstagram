c7278768a8c740fd408d573bd7b5652123700a11
Luis Carlos Rodriguez <clbarrios@uninorte.edu.co>
2020-12-03 21:55:22 -0500

    implementadas pestañas con grid layout en principal

---
+++ scripts/principal.js
@@ -0,0 +1,21 @@
+function openTab(id) {
+    var i, tabcontents, tabs, activeTab;
+    tabcontents = document.getElementsByClassName("tab-content");
+
+    for (i = 0; i < tabcontents.length; i++) {
+        tabcontents[i].style.display = "none";
+    }
+
+    tabs = document.getElementsByClassName("tab")
+    for (i = 0; i < tabs.length; i++) {
+        tabs[i].className = tabs[i].className.replace(" active", "");
+    }
+
+    console.log(id+"-content");
+    activeTab = document.getElementById(id + "-content");
+    activeTab.style.display = "block";
+
+    console.log(id+"-tab");
+    activeTab = document.getElementById(id + "-tab");
+    activeTab.className += " active";
+}
