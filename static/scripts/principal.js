f8d0a58907c247cc3ad6da29c590bec52463ea78
Luis Carlos <luiscarrodba97@gmail.com>
2020-12-08 11:59:21 -0500

    modal de agregar imagen muestra imagen centrada y dimennsionada de acuerdo a su relación de aspecto al cargarla

--- static/scripts/principal.js
+++ static/scripts/principal.js
@@ -22,3 +22,27 @@ function openTab(id) {
     activeTab.className += " active";
 }
 
+function displayImg(event) {
+    // objeto imagen para consultar dimensiones antes de mostrarla
+    var img = new Image();
+    // cuando cargue la imagen ajustar tamaño a mostrar según las dimensiones de la imagen
+    img.onload = function() {
+        var h = img.height;
+        var w = img.width;
+        console.log("img size: " + this.width + "x" + this.height);
+        var dispImg = document.getElementById("imgDisplay");
+        dispImg.src = this.src;
+        if (w >= h) {
+            dispImg.style.width = "100%";
+            dispImg.style.height = "";
+        } else {
+            dispImg.style.height = "100%";
+            dispImg.style.width = "";
+        }
+        document.getElementById("imgPlaceholder").style.display = "none";
+        dispImg.style.display = "block";
+        //dispImg.style.margin = "auto";
+    }
+    // asignar imagen cargada al objeto imagen
+    img.src = URL.createObjectURL(event.target.files[0])
+}
