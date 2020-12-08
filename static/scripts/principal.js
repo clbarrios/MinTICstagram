1bc732a575ed4555b58e6490071e004206b4fdf9
paula-web <paulaandrearamospe@gmail.com>
2020-12-08 14:18:38 -0500

    funcionamiento de las pestañas de abajo pero falta aun

--- static/scripts/principal.js
+++ static/scripts/principal.js
@@ -1,7 +1,7 @@
 
 
 function openTab(id) {
-    var i, tabcontents, tabs, activeTab;
+    var i, tabcontents, tabs, activeTab, tabscon, tabss;
     tabcontents = document.getElementsByClassName("tab-content");
 
     for (i = 0; i < tabcontents.length; i++) {
@@ -13,13 +13,26 @@ function openTab(id) {
         tabs[i].className = tabs[i].className.replace(" active", "");
     }
 
-    console.log(id+"-content");
-    activeTab = document.getElementById(id + "-content");
-    activeTab.style.display = "block";
+    tabscon = document.getElementsByClassName("tabb-contentt");
+    for (i = 0; i < tabscon.length; i++) {
+        tabscon[i].style.display = "none";
+    }
 
-    console.log(id+"-tab");
-    activeTab = document.getElementById(id + "-tab");
-    activeTab.className += " active";
+    tabss = document.getElementsByClassName("tabb")
+    for (i = 0; i < tabss.length; i++) {
+        tabss[i].className = tabss[i].className.replace(" active", "");
+    }
+
+
+    console.log(id+"-content");
+  
+        activeTab = document.getElementById(id + "-content");
+        activeTab.style.display = "block";
+
+        console.log(id+"-tab");
+        activeTab = document.getElementById(id + "-tab");
+        activeTab.className += " active";
+
 }
 
 
