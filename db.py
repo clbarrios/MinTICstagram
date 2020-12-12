b4bcc4bebfbd7212276d0d0fcaa300ba703513ba
Luis Carlos <clbarrios@uninorte.edu.co>
2020-12-11 21:17:37 -0500

    metodos sql para tabla Usuarios

--- db.py
+++ db.py
@@ -15,3 +15,56 @@ def desconectar():
         db = g.pop('db', None)
         if db is not None:
             db.close()
+
+# Para insertar un usuario nuevo
+def insertar_usuario(nombre, correo, contraseña):
+    query = f"INSERT INTO Usuarios (nombre, correo, contraseña) VALUES ('{nombre}', '{correo}', '{contraseña}');"
+    try:
+        con = conectar()
+        cursor = con.cursor()
+        cursor.execute(query)
+        con.commit()
+        con.close()
+    except Error:
+        print(Error)
+
+# Para autenticar un usuario
+def consultar_usuario(nombre, contraseña):
+    query = f"SELECT * FROM Usuarios WHERE nombre='{nombre}' contraseña='{contraseña}';"
+    try:
+        con = conectar()
+        cursor = con.cursor()
+        cursor.execute(query)
+        usuario = cursor.fetchall()
+        con.close()
+        return True if (len(usuario) > 0) else False
+    except Error:
+        print(Error)
+
+# para validar si un correo o un nombre de usuario está registrado
+def validar_nuevo_usuario(nombre, correo):
+    query = f"SELECT * FROM Usuarios WHERE nombre='{nombre}' OR correo='{correo}';"
+    try:
+        con = conectar()
+        cursor = con.cursor()
+        cursor.execute(query)
+        usuarios = cursor.fetchall()
+        con.close()
+        return True if (len(usuarios) > 0) else False
+        
+    except Error:
+        print(Error)
+
+# Para obtener el id de un usuario
+def getIdUsuario(nombre):
+    query = f"SELECT id FROM Usuario WHERE nombre='{nombre}';"
+    try:
+        con = conectar()
+        cursor = con.cursor()
+        cursor.execute(query)
+        res = cursor.fetchone()
+        con.close()
+        return res[0]
+        
+    except Error:
+        print(Error)
